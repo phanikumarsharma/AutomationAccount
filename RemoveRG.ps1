31dc363457dcce71f88119ae7cc5f76b72c0eac6
phanikumarsharma <ramaduguphanikumar@gmail.com>
2018-08-13 05:43:57 -0700

    test

---
+++ RemoveRG.ps1
@@ -0,0 +1,34 @@
+﻿Param(
+
+    [Parameter(Mandatory = $True)]
+    [ValidateNotNullOrEmpty()]
+    [string] $SubscriptionId,
+    
+    [Parameter(Mandatory = $True)]
+    [ValidateNotNullOrEmpty()]
+    [string] $ResourceGroupName,
+
+    [Parameter(Mandatory = $True)]
+    [ValidateNotNullOrEmpty()]
+    [string] $UserName,
+
+    [Parameter(Mandatory = $True)]
+    [ValidateNotNullOrEmpty()]
+    [string] $Password
+)
+
+Write-Host "Logging in to Azure Rm Account"
+$Psswd = $Password | ConvertTo-SecureString -asPlainText -Force
+$Credential = New-Object System.Management.Automation.PSCredential($UserName,$Psswd)
+Login-AzureRmAccount -Credential $Credential
+
+Write-Host "Select Azure Subscription"
+
+Select-AzureRmSubscription -SubscriptionId $SubscriptionId
+
+Write-Host "Remove the Resource Group"
+
+Remove-AzureRmResourceGroup -Name $ResourceGroupName -Force
+
+Write-Host "Resource Group has been removed Successfully"
+
